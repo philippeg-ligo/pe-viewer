5fea9e2e6c9ba34c21d4555320c117b36c6e042b
Philippe Grassia <philippe.grassia@ligo.org>
2023-01-25 17:03:27 -0800

    brand that pic

--- docker/brand-image.py
+++ docker/brand-image.py
@@ -4,5 +4,5 @@ import datetime
 img = Image.open('img/black-hole-ellipse.png')
 d1 = ImageDraw.Draw(img)
 now = datetime.datetime.now()
-d1.text((28, 460), f"peviewer build on {now}", fill=(128, 128, 128))
+d1.text((28, 460), f"peviewer:  {now}", fill=(128, 128, 128))
 img.save('img/black-hole-ellipse.png')
